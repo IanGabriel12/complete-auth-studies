c888ce36a6a7c7f7de43570185a15c529d18ff34
Ian Gabriel <ianeigor12@gmail.com>
2024-10-01 22:00:00 -0300

    feat: adding login screen

--- complete-auth-studies-frontend/src/App.vue
+++ complete-auth-studies-frontend/src/App.vue
@@ -1,85 +1,33 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="flex h-full">
+    <div class="bg-red-600 w-4/6 h-f"></div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <main id="login" class="flex mt-60 justify-center w-2/6">
+      
+      <form @submit.prevent class="w-96">
+        <h1 class="text-red-600 font-bold text-5xl">Welcome back</h1>
+        <span class="text-sm text-gray-500">Do not have an account? <a class="text-red-500 underline">Create one here.</a></span>
+        
+        <div class="field mt-8">
+          <label for="username" class="block text-red-600 text-md font-semibold">Username</label>
+          <input type="text" name="username" class="p-1 border-red-200 border-2 rounded-md mt-1 w-full shadow-md">
+        </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <div class="field mt-4">
+          <label for="password" class="text-red-600 text-md font-semibold">Password</label>
+          <input type="password" name="password" id="password" class="p-1 border-red-200 border-2 rounded-md mt-1 w-full shadow-md">
+          <a class="text-sm text-red-500 underline block mt-1">Forgot my password</a>
+        </div>
 
-  <RouterView />
+        <button class="mt-6 w-full bg-red-600 p-2 text-white rounded-md">Login</button>
+      </form>
+      
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
